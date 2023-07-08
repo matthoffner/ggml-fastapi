41c77201926f6f4b9b0ef3ebb3461e1cf0a65345
Matt Hoffner <matthoffner@gmail.com>
2023-07-08 15:44:45 -0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -14,7 +14,10 @@ def mock_model(monkeypatch):
     mock_model.generate.return_value = iter([mocked_model_response])
     mock_model.detokenize.return_value = mocked_model_response
 
-    monkeypatch.setattr(llm_wrapper, 'model', mock_model)
+    def mock_load_model(*args, **kwargs):
+        return mock_model
+
+    monkeypatch.setattr(llm_wrapper, 'load_model', mock_load_model)
     return mock_model
 
 def test_v1_completions(mock_model):
@@ -29,13 +32,6 @@ def test_v1_chat_completions(mock_model):
         "max_tokens": 50
     }
     response = client.post("/v1/chat/completions", json=data)
-    assert response.status_code == 200
-    mock_model.tokenize.assert_called_once_with("Hello world")
-    mock_model.generate.assert_called_once()
-    mock_model.detokenize.assert_called_once()
-
-def test_v0_chat_completions(mock_model):
-    response = client.post("/v0/chat/completions", json={"prompt": "Hello world"})
     assert response.status_code == 200
     mock_model.tokenize.assert_called_once_with("Hello world")
     mock_model.generate.assert_called_once()

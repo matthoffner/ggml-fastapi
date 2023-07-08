c4650de3e04ade7dd37f4a67de55d08a1ac284e9
Matt Hoffner <matthoffner@gmail.com>
2023-07-08 14:38:37 -0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 from unittest.mock import Mock, patch
 import pytest
 from fastapi.testclient import TestClient
-from main import app # assuming your code is in a file called main.py
+from main import app
 
 client = TestClient(app)
 
@@ -13,14 +13,19 @@ def mock_llm(monkeypatch):
     mock_llm.tokenize.return_value = "mocked tokens"
     mock_llm.generate.return_value = iter([mocked_model_response])
     mock_llm.detokenize.return_value = mocked_model_response
+
+    def mock_from_pretrained(*args, **kwargs):
+        return mock_llm
+
+    monkeypatch.setattr("ctranformers.AutoModelForCausalLM.from_pretrained", mock_from_pretrained)
     monkeypatch.setattr("main.llm", mock_llm)
     return mock_llm
 
 def test_v1_completions(mock_llm):
     response = client.post("/v1/completions", json={"prompt": "Hello world"})
     assert response.status_code == 200
-    # Assert that the llm __call__ method was used correctly
-    mock_llm.__call__.assert_called_once_with("Hello world")
+    mock_llm.tokenize.assert_called_once_with("Hello world")
+    mock_llm.generate.assert_called_once_with("mocked tokens")
 
 def test_v1_chat_completions(mock_llm):
     data = {
@@ -29,9 +34,9 @@ def test_v1_chat_completions(mock_llm):
     }
     response = client.post("/v1/chat/completions", json=data)
     assert response.status_code == 200
-    # Assert that the mock_llm's methods were called correctly
     mock_llm.tokenize.assert_called_once_with("Hello world")
     mock_llm.generate.assert_called_once_with("mocked tokens")
+    mock_llm.detokenize.assert_called_once()
 
 def test_v2_chat_completions(mock_llm):
     data = {
@@ -40,14 +45,13 @@ def test_v2_chat_completions(mock_llm):
     }
     response = client.post("/v2/chat/completions", json=data)
     assert response.status_code == 200
-    # Assert that the mock_llm's methods were called correctly
     mock_llm.tokenize.assert_called_once_with("Hello world")
     mock_llm.generate.assert_called_once_with("mocked tokens")
+    mock_llm.detokenize.assert_called_once()
 
 def test_v0_chat_completions(mock_llm):
     response = client.post("/v0/chat/completions", json={"prompt": "Hello world"})
     assert response.status_code == 200
-    # Assert that the llm tokenize and generate methods were called correctly
     mock_llm.tokenize.assert_called_once_with("Hello world")
     mock_llm.generate.assert_called_once_with("mocked tokens")
-
+    mock_llm.detokenize.assert_called_once()
